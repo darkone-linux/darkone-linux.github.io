d8c6f23b4e88d3eded29fd0bb2eb018137e2c4fa
Darkone Linux <darkone@darkone.yt>
2025-12-15 23:25:52 -0300

    Doc generation improvement

--- chunks/content-assets_E6iwP9kT.mjs
+++
@@ -1,5 +0,0 @@
-import { _ as __ASTRO_IMAGE_IMPORT_Z26UXhl } from './nix-logo_wI34PJpX.mjs';
-
-const contentAssets = new Map([["../../assets/nix-logo.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx", __ASTRO_IMAGE_IMPORT_Z26UXhl]]);
-
-export { contentAssets as default };
